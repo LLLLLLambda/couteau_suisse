7cbc5c04a7dda147d4c723fd976ee43aa0d90b3a
LLLLLLambda <legollas1908@hotmail.fr>
2020-02-05 17:22:49 +0100

    button upload

--- couteau_suisse/src/main/java/com/mycompany/couteau_suisse/manager/Manager.java
+++ couteau_suisse/src/main/java/com/mycompany/couteau_suisse/manager/Manager.java
@@ -8,16 +8,47 @@ package com.mycompany.couteau_suisse.manager;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.kernel.utils.PdfMerger;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import org.primefaces.shaded.commons.io.FilenameUtils;
 
 /**
  *
  * @author eddy.parisi
  */
+@Named
+@RequestScoped
 public class Manager {
-    PdfDocument pdfDownload = null;
+    PdfDocument pdfDownload;
+    private UploadedFile file;
+    
+    public void upload() throws IOException {
+        if (file != null) {
+            //préciser 
+            Path path = Paths.get("C:\\images\\");
+            String filename = FilenameUtils.getBaseName(file.getFileName());
+            String extension = FilenameUtils.getExtension(file.getFileName());
+            System.out.println(path + " " + filename + "." + extension);
+            Path fichierTempo = Files.createTempFile(path, filename, "." + extension);
+            
+            try (InputStream input = file.getInputstream()) {
+                Files.copy(input, fichierTempo, StandardCopyOption.REPLACE_EXISTING);
+                FacesMessage message = new FacesMessage(file.getFileName() + " : is uploaded.");
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }
+    }
     
     public void fusionnerDeuxPdf() throws IOException{
         PdfWriter writer = new PdfWriter(new File("src/main/webapp/resources/pdf/pdf7.pdf"));
@@ -32,5 +63,20 @@ public class Manager {
     }
     
     public static void main(String[] args) throws IOException {
+    }
+    
+    
+    
+    public UploadedFile getFile() {
+        return file;
+    }
+    
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+    
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
