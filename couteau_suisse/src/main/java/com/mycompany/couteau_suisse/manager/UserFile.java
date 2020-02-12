5a3c468af6f7461926d2f9218c1ed98ae33b5757
MathildeBarre <errabmathilde@gmail.com>
2020-02-12 10:46:45 +0100

    userfile

---
+++ couteau_suisse/src/main/java/com/mycompany/couteau_suisse/manager/UserFile.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.couteau_suisse.manager;
+
+/**
+ *
+ * @author errab
+ */
+public class UserFile {
+    private String name;
+    
+    public UserFile(String fileName) {
+        setName(fileName);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
